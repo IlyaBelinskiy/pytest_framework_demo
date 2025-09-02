63e3ce4020cbd7659e11dea9840cc1ca1ba0b47c
Ilya Belinskiy <68616504+IlyaBelinskiy@users.noreply.github.com>
2025-09-02 18:41:25 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,15 @@ from pytest import fixture
 
 from selenium  import webdriver
 
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @fixture(scope='function')
 def chrome_browser():
-    service = Service(executable_path='./chromedriver')
-    browser = webdriver.Chrome(service=service)
+    options = webdriver.ChromeOptions()
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     return browser
     # yield browser
 
