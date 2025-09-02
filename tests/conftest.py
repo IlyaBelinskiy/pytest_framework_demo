03ab173a1570c0b74f13fad0bb0190ebf81f8c5b
Ilya Belinskiy <68616504+IlyaBelinskiy@users.noreply.github.com>
2025-09-02 15:12:53 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selenium  import webdriver
 
 @fixture(scope='function')
 def chrome_browser():
-    browser = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver/chromedriver-linux64/chromedriver')
+    browser = webdriver.Chrome()
     # return browser
     yield browser
 
