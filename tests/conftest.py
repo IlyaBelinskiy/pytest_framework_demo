8c9bb936b6aaa3c9ee13b1bc9728ff03bc24685a
Ilya Belinskiy <68616504+IlyaBelinskiy@users.noreply.github.com>
2025-09-02 18:36:17 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.service import Service
 
 @fixture(scope='function')
 def chrome_browser():
-    service = Service(executable_path='/usr/local/bin/chromedriver')
+    service = Service(executable_path='./chromedriver')
     options = webdriver.ChromeOptions()
     browser = webdriver.Chrome(service=service, options=options)
     return browser
